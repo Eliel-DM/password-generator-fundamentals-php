6bd93d7f8cccab5f047ab1ccb05974592d6b78a4
Eliel-DM <elieldiasmatos@gmail.com>
2026-08-11 21:46:39 -0300

    Iniciando a criação de um validador de segurança para a senha

--- password.php
+++ password.php
@@ -30,3 +30,32 @@ function passowordGenerator(int $passwordSize, bool $includeNumeric, bool $inclu
 
     return $passwordString;
 }
+
+
+
+function passwordValidator(string $password): string {
+    $sizePassword = strlen($password);
+
+    return "12312";
+}
+
+// Função para validar se a senha tem os  caracteres desejáveis 
+function checkCaractere(string $password): int {
+    global $stringContentNumeric, $stringContentLowerCaseStrings, $stringContentUpperCaseStrings, $stringContentSpecialCharacters;
+    $countCaractereClass = 0;
+    $passwordArray = str_split($password);
+
+    foreach ($passwordArray as $letraOfPassoword) {
+        $stringContentNumericArray = str_split($stringContentNumeric);
+        // var_dump($passwordArray);
+        foreach ($stringContentNumericArray as $caractereNumeric) {
+            if ($letraOfPassoword == $caractereNumeric) {
+                $countCaractereClass += 1;
+                break;
+            } else {
+            }
+        }
+    }
+
+    return $countCaractereClass;
+}
---
+++ teste.php
@@ -0,0 +1,8 @@
+<?php
+
+// Vou utilizar esse arquivo para chamar algumas funções de teste e validar alguns comportamentos.
+
+include __DIR__ . "/password.php";
+
+$counter = checkCaractere("oi1");
+echo $counter;
